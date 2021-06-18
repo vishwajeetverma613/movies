e7812e436be4ce836689bc042b2976ad64605061
vishwajeetverma613 <vishwajeetverma.5@gmail.com>
2021-06-18 21:44:31 +0530

    first step completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './Components/Header/Header';
+import Movies from './Components/Movies/Movies';
+import axios from "axios";
+import { API_KEY, API_URL } from "./API/secrets.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { 
+    moviesData: [],
+    currentMovie:"batman"
+   }
+
+  async componentDidMount() {
+    // API call  always call API in current did mount function
+    // params => api key , page , query
+    // https://api.themoviedb.org/3/search/movie?api_key=bdd243ea847239dc0799805e63e189f0&query=avengers&page=1&include_adult=false
+    let data = await axios.get(API_URL + "/search/movie", {
+      params: { api_key: API_KEY, page: 1, query: this.state.currentMovie },
+    });
+    let moviesData = data.data.results;
+    this.setState({
+      moviesData:moviesData
+    })
+  }
+
+  setMovies = async(newMovieName) =>{
+    let data = await axios.get(API_URL + "/search/movie", {
+      params: { api_key: API_KEY, page: 1, query: newMovieName },
+    });
+    let moviesData = data.data.results.slice(0,10);
+    this.setState({
+      moviesData:moviesData,
+      currentMovie:newMovieName
+    })
+  }
+
+
+  render() { 
+    return ( <div className = "App">Hello from App
+    <Header setMovies={this.setMovies}></Header>
+    <Movies movies = {this.state.moviesData}></Movies></div>
+     );
+  }
 }
-
+ 
 export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import "./header.css";
+
+class Header extends Component {
+    state = {
+        newMovieName: "",
+      };
+    
+      handleOnChange = (e) => {
+        let value = e.target.value;
+        this.setState({
+          newMovieName: value,
+        });
+      };
+    
+      handleKeyPress = (e) => {
+        if (e.key == "Enter") {
+          this.props.setMovies(this.state.newMovieName);
+        }
+      };
+    render() { 
+        return (<div className="header">
+        <div className="logo">
+          <img src="logo.svg" alt="" />
+        </div>
+        <div className="search-btn">
+          <input
+            className="search-movies"
+            value={this.state.newMovieName}
+            type="text"
+            placeholder="Search"
+            onChange={this.handleOnChange}
+            onKeyPress={this.handleKeyPress}
+          />
+        </div>
+      </div>
+             );
+    }
+}
+ 
+export default Header;
